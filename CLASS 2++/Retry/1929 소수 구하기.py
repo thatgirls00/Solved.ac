b02f53d118e940b45d8dc73b61880c6f4cae4755
wjsgustn201@gmail.com <wjsgustn201@gmail.com0>
2024-01-13 18:51:56 +0900

    _

--- CLASS 2++/Retry/1929 소수 구하기.py
+++ CLASS 2++/Retry/1929 소수 구하기.py
@@ -1,11 +1,13 @@
-# 보류
+def num(M, N):
+    for i in range(M, N + 1):
+        if i > 1:
+            s = True
+            for j in range(2, int(i ** 0.5) + 1):
+                if i % j == 0:
+                    s = False
+                    break
+            if s:
+                print(i)
 
 M, N = map(int, input().split())
-
-for i in range(M, N+1):
-    if i > 1:
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                break
-            else:
-                print(i)
+num(M, N)
