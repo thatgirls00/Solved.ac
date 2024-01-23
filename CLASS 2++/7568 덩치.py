0a6ae212d9033a1e78d3d23916ca3ce31565d7e7
wjsgustn201@gmail.com <wjsgustn201@gmail.com0>
2024-01-23 12:30:02 +0900

    _

---
+++ CLASS 2++/7568 덩치.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    lst.append((x, y))
+
+for i in lst:
+    rank = 1
+    for j in lst:
+        if i[0] < j[0] and i [1] < j[1] :
+            rank += 1
+    print(rank, end = " ")
