2aa54f47a460a731c54fd1f0d0bf6cf2ac8dbfa4
wjsgustn201@gmail.com <wjsgustn201@gmail.com0>
2024-02-03 14:19:06 +0900

    _

---
+++ CLASS 2++/11866 요세푸스 문제 0.py
@@ -0,0 +1,16 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+idx = 0
+que = [i for i in range(1, n+1)]
+res = []
+
+while que:
+    idx += k -1
+    if idx >= len(que):
+        idx %= len(que)
+    res.append(str(que.pop(idx)))
+
+print("<", ", ".join(res), ">", sep="")
+
