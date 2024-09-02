47c459c378691cf5d7aff92950fdb3f0b383bc09
Soo <109068985+thatgirls00@users.noreply.github.com>
2024-09-03 08:52:02 +0900

    _

--- 9086 문자열.py
+++ 9086 문자열.py
@@ -2,5 +2,8 @@ n = int(input())
 
 for i in range(n):
     word = list(input())
+    # list는 입력받은 문자열을 문자들의 리스트로 변환
+    # str은 input()과 같이 문자열 그대로  반환
+    # list는 문자열을 문자 리스트로 변환하여 더 많은 문자 단위 조작 가능
 
     print(word[0] + word[-1])
